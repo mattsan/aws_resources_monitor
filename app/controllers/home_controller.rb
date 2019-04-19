55a60af5d6c23669318105678edde3d104b2cc3c
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2019-04-19 17:44:30 +0900

    add ec2 states page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @instances = Ec2.new.describe_instances.sort_by {|i| i.launch_time }
   end
 end
--- app/models/ec2.rb
+++ app/models/ec2.rb
@@ -1,7 +1,33 @@
 class Ec2
+  Instance = Struct.new('Instance', :id, :name, :state, :launch_time)
   def describe_instances
     client = Aws::EC2::Client.new(region: 'ap-northeast-1')
 
-    client.describe_instances
+    filters = [
+      {
+        name: 'instance-state-name',
+        values: ['running']
+      },
+      {
+        name: 'tag:stage',
+        values: ['staging']
+      },
+      {
+        name: 'tag:component',
+        values: %w(hr recruiter entry admin ops)
+      }
+    ]
+    client
+      .describe_instances(filters: filters)
+      .reservations
+      .flat_map(&:instances)
+      .map {|i|
+        Instance.new(
+          i.instance_id,
+          i.tags.select {|tag| tag.key == 'Name' }.map(&:value).join,
+          i.state.name,
+          i.launch_time.localtime
+        )
+      }
   end
 end
